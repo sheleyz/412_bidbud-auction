3bf3c49ff949be8164ba69a6a73d89adc4f3b57e
Seth Wheeler <sethwhee@iu.edu>
2022-04-19 19:51:07 +0000

    mail cloud function working

--- backend/mail_function/index.js
+++ backend/mail_function/index.js
@@ -1,38 +1,35 @@
-const {Firestore} = require('@google-cloud/firestore');
 const sgMail = require('@sendgrid/mail');
 
-const firestore = new Firestore();
-
-exports.sendMail = async (event, context) => {      
-
+exports.sendMail = (event, context) => {
+    
     userBid = {};
 
     userBid.email = event.value.fields.email.stringValue;
     userBid.bid_amount = event.value.fields.bid_amount.stringValue;
-    userBid.item_id = event.value.fields.itemID.stringValue;            
+    userBid.item_id = event.value.fields.itemID.stringValue;
 
     // API KEY
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey('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');
 
     // Prep Email
 
-    const mailText = "A new bid has been made for $" + userBid.bid_amount + " on item: " + userBid.item_id;
-
+    const mailText = "A new bid has been made for $" + userBid.bid_amount + " on Item " + userBid.item_id;    
+    
+    
     const msg = {
         to: userBid.email,
-        from: process.env.SENDGRID_SENDER,
-        subject: "New Bid Made - BidBud",
+        from: 'sethwhee@iu.edu',
+        subject: "BidBud - New Bid Made",
         text: mailText,
         html: mailText
-    };
-
-    console.log(`Message: ${msg}`);
-
+    };     
+    
     // Send Email
+    
     sgMail
     .send(msg)
     .then(() =>{}, error => {
         console.error(error);
-    });
+    });    
     
-  };
+};
